8d0fac1c0ee74c8505e983e9f94e9155505e2373
marioweid <mario.weidner@gmx.de>
2023-12-02 18:08:22 +0100

    adde logik to parse row to hashmap

---
+++ day_02_a/src/idea.md
@@ -0,0 +1,8 @@
+# Flos methode
+
+# Game id Merken : index des tuples
+# 3er tuple aus den 3 Farben
+# 3er häpchen in validater funkion die validiert
+# Für jede Zeile
+#  nur gültig wenn alle spiele valide waren
+#  summe der gültigen spiel ids
--- day_02_a/src/main.rs
+++ day_02_a/src/main.rs
@@ -1,4 +1,5 @@
 use core::num;
+use std::collections::btree_map::Values;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -15,11 +16,7 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn import_game_data() -> GamesData{
-
-
-    let ret = GamesData::new();
-    ret
+fn import_game_data(row: &str) -> GamesData{
     // // File hosts.txt must exist in the current path
     // if let Ok(lines) = read_lines("./src/example.txt") {
     //     // Consumes the iterator, returns an (Optional) String
@@ -31,6 +28,31 @@ fn import_game_data() -> GamesData{
     //         }
     //     }
     // }
+
+    // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    let mut ret: HashMap<String, HashMap<String, Vec<u32>>> = GamesData::new();
+    let mut splitted: std::str::Split<'_, &str> = row.split(": ");
+    // Just yolo unwrap the values 
+    // Game 3
+    let game_key: &str = splitted.next().unwrap();
+    // 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    let game_string: &str = splitted.next().unwrap();
+    // 8 green, 6 blue, 20 red
+    // 5 blue, 4 red, 13 green; 5 green, 1 red
+    let splitted_sets: Vec<&str> = game_string.split("; ").into_iter().collect();
+    for set in splitted_sets {
+        let color_tuples: std::str::Split<'_, &str> = set.split(", ");
+        for tuple in color_tuples {
+            let mut values: std::str::Split<'_, &str> = tuple.split(" ");
+            let digit = values.next().unwrap().parse::<u32>().unwrap();
+            let color_key = values.next().unwrap();
+
+            let game_entry: &mut HashMap<String, Vec<u32>> = ret.entry(game_key.to_string()).or_insert(HashMap::new());
+            let color_entry: &mut Vec<u32> = game_entry.entry(color_key.to_string()).or_insert(Vec::new());
+            color_entry.push(digit)
+        }
+    }
+    ret
 }
 
 fn create_color_counts(pairs: Vec<(&str, u32)>) -> ColorCounts {
@@ -45,13 +67,13 @@ fn create_color_counts(pairs: Vec<(&str, u32)>) -> ColorCounts {
     color_counts
 }
 
-fn valid_row(row: Option<&ColorCounts>, possible_values: HashMap<&str, u32>) -> bool{
+fn is_valid_row(row: Option<&ColorCounts>, possible_values: &HashMap<&str, u32>) -> bool{
     // check if the row exists 
     let ret: bool = match row {
         Some(row_values) => {
             let all_smaller = row_values.iter().all(|(key, values)| {
                 return possible_values.get(key.as_str()).map_or(false, |possible_val| {
-                    values.iter().all(|val| val < possible_val) 
+                    values.iter().all(|val| val <= possible_val) 
                 });
             });
             all_smaller
@@ -68,7 +90,6 @@ fn game_string_to_int(game_string: String) -> i32 {
         .split_whitespace()
         .last()
         .map(|val| val.parse::<i32>());
-        // .unwrap_or_else(|| Err("Cant Parse game string!".to_string()));
 
     match parse_result {
         Some(Ok(number)) => number,
@@ -77,8 +98,17 @@ fn game_string_to_int(game_string: String) -> i32 {
 }
 
 fn main() {
-    let mut games_data: GamesData = HashMap::new();
 
+    let row = "Game 3: 8 green, 6 blue, 10 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+    let color_tuples: HashMap<&str, u32> = HashMap::from_iter([("red", 12u32),("green", 13u32),("blue", 14u32)]);
+    let game_row: HashMap<String, HashMap<String, Vec<u32>>> = import_game_data(row);
+    let is_valid: bool = is_valid_row(game_row.get("Game 3"), &color_tuples);
+
+    println!("is_valid: {}", is_valid);    
+    println!("Breakpoint");
+    
+    // ------------------------
+    let mut games_data: GamesData = HashMap::new();
     games_data.insert("Game 1".to_string(), create_color_counts(vec![("blue", 3), ("red", 4), ("red", 1), ("green", 2), ("blue", 6), ("green", 2)]));
     games_data.insert("Game 2".to_string(), create_color_counts(vec![("blue", 1), ("green", 2), ("green", 3), ("blue", 4), ("red", 1), ("green", 1), ("blue", 1)]));
     games_data.insert("Game 3".to_string(), create_color_counts(vec![("green", 8), ("blue", 6), ("red", 20), ("blue", 5), ("red", 4), ("green", 13), ("green", 5), ("red", 1)]));
@@ -87,29 +117,27 @@ fn main() {
 
 
     // Check if there is a match for a given condition
-    let color_tuples: HashMap<&str, u32> = HashMap::from_iter([("red", 12u32),("green", 13u32),("blue", 14u32)]);
 
-    let mut game_idx: Vec<i32> = Vec::new();
+    let mut valid_rows: Vec<i32> = Vec::new();
     for i in 1..6 {
-        let game_number: String = format!("Game {}", i);
-        let row: Option<&HashMap<String, Vec<u32>>> = games_data.get(game_number.as_str());
-        let valid = valid_row(row, color_tuples.clone());
-        if valid {
-            game_idx.push(game_string_to_int(game_number.clone()));
+        let row_index: String = format!("Game {}", i);
+        let row: Option<&HashMap<String, Vec<u32>>> = games_data.get(row_index.as_str());
+        let is_valid: bool = is_valid_row(row, &color_tuples);
+        if is_valid {
+            valid_rows.push(game_string_to_int(row_index.clone()));
         }
     }
 
-    let cloned_indexes = game_idx.clone();
-    for index in game_idx {
+    for index in &valid_rows {
         println!("{}", index);
     }
 
-    let the_sum: i32 = cloned_indexes.into_iter().sum();
+    let the_sum: i32 = valid_rows.into_iter().sum();
     println!("The sum: {}", the_sum);
 
 
 
-
+    // ------------------------
 
 
 
@@ -132,7 +160,7 @@ fn main() {
     //     println!("col_match: {}", col_match)
 
 
-    
+
 
     //     // if Some(actual_colors_counts) {
     //     //     if actual_colors_counts.iter().all(|val| val < &count_to_check) {
