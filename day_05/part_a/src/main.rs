c9d8870ed7b7182058f8d56f5a6b0d2e75176949
Weidner, Mario <mario.weidner@hm.edu>
2023-12-05 10:01:17 +0100

    removed warnings

--- day_05/part_a/src/main.rs
+++ day_05/part_a/src/main.rs
@@ -86,27 +86,27 @@ fn main() {
     let soil_entry_3 = SourceDestinationEntry::new(39,0,15);
 
     // fertilizer
-    let fert_entry_1 = crate::SourceDestinationEntry::new(49,53,8);
-    let fert_entry_2 = crate::SourceDestinationEntry::new(0, 11, 42);
-    let fert_entry_3 = crate::SourceDestinationEntry::new(42, 0 , 7);
-    let fert_entry_4 = crate::SourceDestinationEntry::new(57, 7, 4);
+    let fert_entry_1 = SourceDestinationEntry::new(49,53,8);
+    let fert_entry_2 = SourceDestinationEntry::new(0, 11, 42);
+    let fert_entry_3 = SourceDestinationEntry::new(42, 0 , 7);
+    let fert_entry_4 = SourceDestinationEntry::new(57, 7, 4);
 
     // water
-    let water_entry_1 = crate::SourceDestinationEntry::new(88, 18, 7);
-    let water_entry_2 = crate::SourceDestinationEntry::new(18, 25, 70);
+    let water_entry_1 = SourceDestinationEntry::new(88, 18, 7);
+    let water_entry_2 = SourceDestinationEntry::new(18, 25, 70);
 
     // light
-    let light_entry_1 = crate::SourceDestinationEntry::new(45, 77, 23);
-    let light_entry_2 = crate::SourceDestinationEntry::new(81, 45, 19);
-    let light_entry_3 = crate::SourceDestinationEntry::new(68, 64, 13);
+    let light_entry_1 = SourceDestinationEntry::new(45, 77, 23);
+    let light_entry_2 = SourceDestinationEntry::new(81, 45, 19);
+    let light_entry_3 = SourceDestinationEntry::new(68, 64, 13);
 
     // temperature
-    let temperature_entry_1 = crate::SourceDestinationEntry::new(0, 69, 1);
-    let temperature_entry_2 = crate::SourceDestinationEntry::new(1, 0, 69);
+    let temperature_entry_1 = SourceDestinationEntry::new(0, 69, 1);
+    let temperature_entry_2 = SourceDestinationEntry::new(1, 0, 69);
 
     // humidity
-    let humidity_entry_1 = crate::SourceDestinationEntry::new(60, 56, 37);
-    let humidity_entry_2 = crate::SourceDestinationEntry::new(56, 93, 4);
+    let humidity_entry_1 = SourceDestinationEntry::new(60, 56, 37);
+    let humidity_entry_2 = SourceDestinationEntry::new(56, 93, 4);
 
     let mut seed_map = Map::new("seed", vec![seed_entry_1, seed_entry_2]);
     let mut soil_map = Map::new("soil", vec![soil_entry_1, soil_entry_2, soil_entry_3]);
@@ -114,7 +114,7 @@ fn main() {
     let mut water_map = Map::new("water", vec![water_entry_1, water_entry_2]);
     let mut light_map = Map::new("light", vec![light_entry_1, light_entry_2, light_entry_3]);
     let mut temperature_map = Map::new("temperature", vec![temperature_entry_1, temperature_entry_2]);
-    let mut humidity_map = Map::new("humidity", vec![humidity_entry_1, humidity_entry_2]);
+    let humidity_map = Map::new("humidity", vec![humidity_entry_1, humidity_entry_2]);
 
     // Note: ownership needs the maps to be set bottom up
     temperature_map.set_next_map(humidity_map);
@@ -124,10 +124,10 @@ fn main() {
     soil_map.set_next_map(fert_map);
     seed_map.set_next_map(soil_map);
 
-    let seed_1 = seed_map.source_to_destination(79);
-    let seed_2 = seed_map.source_to_destination(14);
-    let seed_3 = seed_map.source_to_destination(55);
-    let seed_4 = seed_map.source_to_destination(13);
+    let _seed_1 = seed_map.source_to_destination(79);
+    let _seed_2 = seed_map.source_to_destination(14);
+    let _seed_3 = seed_map.source_to_destination(55);
+    let _seed_4 = seed_map.source_to_destination(13);
 
     println!("...")
 }
