7a855e14b5ad73c91e76adc32c41078cd252f7e7
marioweid <mario.weidner@gmx.de>
2023-12-07 15:30:41 +0100

    added 7/a

--- day_07/part_a/src/main.rs
+++ day_07/part_a/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
-use std::{fs, collections::HashMap};
+use std::{fs, collections::HashMap, cmp::Ordering};
 
+#[derive(Debug, Eq, PartialEq)]
 struct Hand {
     cards: Vec<String>,
     bid: i32,
@@ -35,7 +36,6 @@ impl Hand {
         let count_vals: Vec<i32> = card_counts.values().cloned().collect();
         let mut _ret: Option<&str> = None;
         if count_vals.contains(&5) {_ret = Some("five_oak")}
-        else if count_vals.contains(&5) { _ret = Some("four_oak") }
         else if count_vals.contains(&4) { _ret = Some("four_oak") }
         else if count_vals.contains(&3) & count_vals.contains(&2) { _ret = Some("full_house") }
         else if count_vals.contains(&3) { _ret = Some("three_oak") }
@@ -51,8 +51,53 @@ impl Hand {
         let mapping: HashMap<&str, i32> = HashMap::from([
             ("A", 14), ("K", 13), ("Q", 12), ("J", 11), ("T", 10), ("9", 9),
             ("8", 8),("7", 7), ("6", 6), ("5", 5), ("4", 4), ("3", 3), ("2", 2)]);
-        let power: i32 = self.cards.iter().map(|val| mapping.get(val.as_str()).unwrap()).sum();
+        let card_count: i32 = self.cards.len() as i32;
+        let power: i32 = self.cards.iter().enumerate().map(|(index, val)| mapping.get(val.as_str()).unwrap() * (20i32.pow((card_count as u32) - (index as u32)))).sum();
         return power;
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // First, compare based on type
+        let type_ordering: Ordering = match (self.hand_type.as_str(), other.hand_type.as_str()) {
+            ("five_oak", "five_oak") => Ordering::Equal,
+            ("five_oak", _) => Ordering::Greater,
+            (_, "five_oak") => Ordering::Less,
+            ("four_oak", "four_oak") => Ordering::Equal,
+            ("four_oak", _) => Ordering::Greater,
+            (_, "four_oak") => Ordering::Less,
+            ("full_house", "full_house") => Ordering::Equal,
+            ("full_house", _) => Ordering::Greater,
+            (_, "full_house") => Ordering::Less,
+            ("three_oak", "three_oak") => Ordering::Equal,
+            ("three_oak", _) => Ordering::Greater,
+            (_, "three_oak") => Ordering::Less,
+            ("two_pair", "two_pair") => Ordering::Equal,
+            ("two_pair", _) => Ordering::Greater,
+            (_, "two_pair") => Ordering::Less,
+            ("one_pair", "one_pair") => Ordering::Equal,
+            ("one_pair", _) => Ordering::Greater,
+            (_, "one_pair") => Ordering::Less,
+            ("rank", "rank") => Ordering::Equal,
+            ("rank", _) => Ordering::Greater,
+            (_, "rank") => Ordering::Less,
+            // Continue with other type comparisons...
+            _ => self.hand_type.cmp(&other.hand_type),
+        };
+
+        // If types are equal, compare based on rank
+        if type_ordering == Ordering::Equal {
+            self.power.cmp(&other.power)
+        } else {
+            type_ordering
+        }
+    }    
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -89,17 +134,18 @@ fn read_data(filepath: &str) -> Vec<Hand> {
     return hands;
 }
 
-// 32T3K 765
-// T55J5 684
-// KK677 28
-// KTJJT 220
-// QQQJA 483
 fn main() {
-    let hands:Vec<Hand>  = read_data("../example.txt");
+    let mut hands: Vec<Hand>  = read_data("../input.txt");
+    hands.sort();
 
-    println!("Amount of Hands: {}", hands.len());
-    for hand in &hands{
-        println!("power: {}", hand.power);
+    for (index, mut hand) in hands.iter_mut().enumerate(){
+        hand.rank = Some(index as i32 + 1) ;
     }
-    println!("Breakpoint!")
+
+    let total_winnings: i32 = hands
+    .iter()
+    .map(|h| h.bid * h.rank.unwrap())
+    .sum();
+
+    println!("total_winnings: {}", total_winnings);
 }
