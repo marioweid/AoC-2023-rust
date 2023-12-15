6a576abf3a89177339496e700c56026d7f0925e9
Weidner, Mario <mario.weidner@hm.edu>
2023-12-15 16:52:22 +0100

    added file read

--- day_10/part_a/src/main.rs
+++ day_10/part_a/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
+
+fn read_file(file_path: &str) -> Vec<Vec<&str>>{
+    let output: Vec<Vec<&str>> = Vec::new();
+    let content = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file"); // TODO Split by \n
+    println!("{:?}", content);
+    output
+}
+
 pub fn main() {
+    let content = read_file("../example.txt");
     println!("Day 10 Solution A!");
 }
