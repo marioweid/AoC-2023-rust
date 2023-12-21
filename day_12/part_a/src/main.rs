c8ebd76e8a50efc551e13f9da09196529593ac9a
marioweid <mario.weidner@gmx.de>
2023-12-21 12:29:52 +0100

    day 12 init

---
+++ day_12/part_a/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+fn read_file(file_path: &str) -> (Vec<char>, Vec<i32>) { //TODO REPLACE FUNCTION
+    let content: String =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let grid: Vec<Vec<char>> = content
+        .lines()
+        .map(|line| line.chars().map(|c| c).collect())
+        .collect();
+
+    return grid;
+}
+
+pub fn main() {
+    println!("Part A")
+}
