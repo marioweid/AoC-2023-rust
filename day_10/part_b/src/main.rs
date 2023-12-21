e868b4577a2d0fc2ea67872a7886d3e6349c0218
marioweid <mario.weidner@gmx.de>
2023-12-21 09:26:14 +0100

    added part b

--- day_10/part_b/src/main.rs
+++ day_10/part_b/src/main.rs
@@ -1,86 +1,69 @@
-use std::{
-    collections::{VecDeque, HashSet},
-    fs,
-};
-
-fn read_file(file_path: &str) -> Vec<Vec<char>> {
-    let content: String =
-        fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let grid: Vec<Vec<char>> = content
-        .lines()
-        .map(|line| line.chars().map(|c| c).collect())
-        .collect();
-
-    return grid;
-}
-
-fn find_start_position(grid: &Vec<Vec<char>>) -> Result<(usize, usize), &'static str> {
-    for (r, row) in grid.iter().enumerate() {
-        for (c, col) in row.iter().enumerate() {
-            if *col == 'S' {
-                return Ok((r, c));
-            }
-        }
-    }
-    Err("Cant find Starting-Position inside the grid.")
-}
-
-fn fill(
-    grid: &Vec<Vec<char>>,
-    mut queue: VecDeque<(usize, usize)>,
-    mut vals_loop: HashSet<(usize, usize)>,
-) -> HashSet<(usize, usize)> {
-    while !queue.is_empty() {
-        let (row_idx, col_idx) = queue.pop_back().unwrap();
-        let current_val = grid[row_idx][col_idx];
-
-        if row_idx > 0
-            && "S|JL".contains(current_val)
-            && "|7F".contains(grid[row_idx - 1][col_idx])
-            && !vals_loop.contains(&(row_idx - 1, col_idx))
-        {
-            vals_loop.insert((row_idx - 1, col_idx));
-            queue.push_front((row_idx - 1, col_idx));
-        }
-
-        if row_idx < grid.len() - 1 
-            && "S|7F".contains(current_val)
-            &&  "|JL".contains(grid[row_idx + 1][col_idx]) 
-            && !vals_loop.contains(&(row_idx +1, col_idx)){
-                vals_loop.insert((row_idx + 1, col_idx));
-                queue.push_front((row_idx + 1, col_idx));
-        }
-
-        if col_idx > 0 
-        && "S-J7".contains(current_val)
-        && "-LF".contains(grid[row_idx][col_idx -1])
-        && !vals_loop.contains(&(row_idx, col_idx - 1)) {
-            vals_loop.insert((row_idx, col_idx - 1));
-            queue.push_front((row_idx, col_idx - 1));
-        }
-
-        if col_idx < grid[row_idx].len() - 1 
-        && "S-LF".contains(current_val) 
-        && "-J7".contains(grid[row_idx][col_idx + 1])
-        && !vals_loop.contains(&(row_idx, col_idx + 1)) {
-            vals_loop.insert((row_idx, col_idx +1));
-            queue.push_front((row_idx, col_idx +1));
-        }
-    }
-    return vals_loop;
-}
-
 pub fn main() {
-    let grid: Vec<Vec<char>> = read_file("../input.txt");
-    let (start_row, start_col) = find_start_position(&grid).unwrap();
-    println!("S: ({},{})", &start_row, &start_col);
+    let map = include_bytes!("../../input.txt");
+    let width = map.iter().position(|&b| b == b'\n').unwrap();
+    let start = map.iter().position(|&b| b == b'S').unwrap();
 
-    let mut vals_loop: HashSet<(usize, usize)> = HashSet::new();
-    vals_loop.insert((start_row, start_col));
+    let mut pipes = vec![false; map.len()];
+    let (mut pos, mut dir) = {
+        if matches!(map[start - width - 1], b'|' | b'7' | b'F') {
+            (start - width - 1, Dir::Up)
+        } else if matches!(map[start + width + 1], b'|' | b'L' | b'J') {
+            (start + width + 1, Dir::Down)
+        } else {
+            (start - 1, Dir::Left)
+        }
+    };
 
-    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
-    queue.push_back((start_row, start_col));
+    std::iter::repeat(())
+        .position(|_| unsafe {
+            *pipes.get_unchecked_mut(pos) = true;
+            match (map.get_unchecked(pos), dir) {
+                (b'|', Dir::Down) => pos += width + 1,
+                (b'|', Dir::Up) => pos -= width + 1,
+                (b'-', Dir::Left) => pos -= 1,
+                (b'-', Dir::Right) => pos += 1,
+                (b'L', Dir::Down) | (b'F', Dir::Up) => {
+                    pos += 1;
+                    dir = Dir::Right;
+                }
+                (b'L', Dir::Left) | (b'J', Dir::Right) => {
+                    pos -= width + 1;
+                    dir = Dir::Up;
+                }
+                (b'7', Dir::Up) | (b'J', Dir::Down) => {
+                    pos -= 1;
+                    dir = Dir::Left;
+                }
+                (b'7', Dir::Right) | (b'F', Dir::Left) => {
+                    pos += width + 1;
+                    dir = Dir::Down;
+                }
+                (b'S', _) => return true,
+                (_, _) => unreachable!(),
+            }
+            false
+        })
+        .unwrap();
 
-    let result: HashSet<(usize, usize)> = fill(&grid, queue, vals_loop);
-    println!("loop_vals: {}", result.len() / 2);
+    let mut inside = false;
+    println!(
+        "{}",
+        map.iter()
+            .enumerate()
+            .filter(|(pos, tile)| {
+                let is_pipe = unsafe { *pipes.get_unchecked(*pos) };
+                inside &= pos % (width + 1) != 0;
+                inside ^= is_pipe && matches!(*tile, b'|' | b'F' | b'7');
+                inside && (!is_pipe || **tile == b'.') && (pos % (width + 1) != width)
+            })
+            .count()
+    );
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
 }
