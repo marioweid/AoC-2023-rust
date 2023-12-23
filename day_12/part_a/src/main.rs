d2f1c73656ba4f320a826365d1e663a89d1fc026
marioweid <mario.weidner@gmx.de>
2023-12-23 11:22:11 +0100

    fixed formatting

--- day_12/part_a/src/main.rs
+++ day_12/part_a/src/main.rs
@@ -23,28 +23,32 @@ fn read_file(file_path: &str) -> (Vec<String>, Vec<Vec<i32>>) {
     return (springs, numbers);
 }
 
-fn combinations(springs: String, numbers: &Vec<i32>) -> i32{
-    if springs == ""{
-        return if numbers.is_empty() {1} else {0};
+fn combinations(springs: String, numbers: &Vec<i32>) -> i32 {
+    if springs == "" {
+        return if numbers.is_empty() { 1 } else { 0 };
     }
 
-    if numbers.is_empty(){
-        return if springs.contains("#")  {0} else {1};
+    if numbers.is_empty() {
+        return if springs.contains("#") { 0 } else { 1 };
     }
 
     let mut ret: i32 = 0;
 
-    let first_el: char = springs.chars().nth(0).unwrap();    
-    if ".?".contains(first_el){
+    let first_el: char = springs.chars().nth(0).unwrap();
+    if ".?".contains(first_el) {
         ret += combinations(springs.chars().skip(1).collect(), numbers);
     }
 
-    if "#?".contains(first_el){
+    if "#?".contains(first_el) {
         let n_0: usize = numbers[0] as usize;
-        if n_0 <= springs.len() && !(springs.chars().take(n_0).collect::<String>().contains('.')) && (springs.len() == n_0 || springs.chars().nth(n_0).unwrap() != '#'){
+        if n_0 <= springs.len()
+            && !(springs.chars().take(n_0).collect::<String>().contains('.'))
+            && (springs.len() == n_0 || springs.chars().nth(n_0).unwrap() != '#')
+        {
             ret += combinations(
-                springs.chars().skip(n_0+1).collect(), 
-                &numbers.iter().skip(1).cloned().collect());
+                springs.chars().skip(n_0 + 1).collect(),
+                &numbers.iter().skip(1).cloned().collect(),
+            );
         }
     }
     ret
@@ -53,10 +57,10 @@ fn combinations(springs: String, numbers: &Vec<i32>) -> i32{
 pub fn main() {
     let (springs, numbers) = read_file("../input.txt");
     let mut results: Vec<i32> = Vec::new();
-    for i in 0..springs.len(){
+    for i in 0..springs.len() {
         let res = combinations(springs[i].to_string(), &numbers[i]);
         results.push(res)
     }
-    let sum:  i32 = results.iter().sum();
+    let sum: i32 = results.iter().sum();
     println!("Result: {}", sum);
 }
