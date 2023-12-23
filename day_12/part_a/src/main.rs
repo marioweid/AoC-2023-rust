1c8372dc8f018f131b03579776267064512b289c
marioweid <mario.weidner@gmx.de>
2023-12-23 11:20:29 +0100

    added day 12

--- day_12/part_a/src/main.rs
+++ day_12/part_a/src/main.rs
@@ -1,16 +1,62 @@
 use std::fs;
 
-fn read_file(file_path: &str) -> (Vec<char>, Vec<i32>) { //TODO REPLACE FUNCTION
+fn read_file(file_path: &str) -> (Vec<String>, Vec<Vec<i32>>) {
     let content: String =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let grid: Vec<Vec<char>> = content
-        .lines()
-        .map(|line| line.chars().map(|c| c).collect())
-        .collect();
+    let lines: Vec<&str> = content.lines().collect();
+    let mut springs: Vec<String> = Vec::new();
+    let mut numbers: Vec<Vec<i32>> = Vec::new();
 
-    return grid;
+    lines.iter().for_each(|line| {
+        let mut line_iter = line.split_ascii_whitespace();
+        let spring_config = line_iter.next().map(|s| s.to_string()).unwrap();
+        let number_config: Vec<i32> = line_iter
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|val| val.parse::<i32>().unwrap())
+            .collect();
+        springs.push(spring_config);
+        numbers.push(number_config)
+    });
+
+    return (springs, numbers);
+}
+
+fn combinations(springs: String, numbers: &Vec<i32>) -> i32{
+    if springs == ""{
+        return if numbers.is_empty() {1} else {0};
+    }
+
+    if numbers.is_empty(){
+        return if springs.contains("#")  {0} else {1};
+    }
+
+    let mut ret: i32 = 0;
+
+    let first_el: char = springs.chars().nth(0).unwrap();    
+    if ".?".contains(first_el){
+        ret += combinations(springs.chars().skip(1).collect(), numbers);
+    }
+
+    if "#?".contains(first_el){
+        let n_0: usize = numbers[0] as usize;
+        if n_0 <= springs.len() && !(springs.chars().take(n_0).collect::<String>().contains('.')) && (springs.len() == n_0 || springs.chars().nth(n_0).unwrap() != '#'){
+            ret += combinations(
+                springs.chars().skip(n_0+1).collect(), 
+                &numbers.iter().skip(1).cloned().collect());
+        }
+    }
+    ret
 }
 
 pub fn main() {
-    println!("Part A")
+    let (springs, numbers) = read_file("../input.txt");
+    let mut results: Vec<i32> = Vec::new();
+    for i in 0..springs.len(){
+        let res = combinations(springs[i].to_string(), &numbers[i]);
+        results.push(res)
+    }
+    let sum:  i32 = results.iter().sum();
+    println!("Result: {}", sum);
 }
--- day_12/part_b/src/main.rs
+++ day_12/part_b/src/main.rs
@@ -1,16 +1,81 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
-fn read_file(file_path: &str) -> (Vec<char>, Vec<i32>) { //TODO REPLACE FUNCTION
+fn read_file(file_path: &str) -> (Vec<String>, Vec<Vec<i64>>) {
     let content: String =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let grid: Vec<Vec<char>> = content
-        .lines()
-        .map(|line| line.chars().map(|c| c).collect())
-        .collect();
+    let lines: Vec<&str> = content.lines().collect();
+    let mut springs: Vec<String> = Vec::new();
+    let mut numbers: Vec<Vec<i64>> = Vec::new();
 
-    return grid;
+    lines.iter().for_each(|line| {
+        let mut line_iter = line.split_ascii_whitespace();
+        let spring_config = line_iter.next().map(|s| s.to_string()).unwrap();
+        let number_config: Vec<i64> = line_iter
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|val| val.parse::<i64>().unwrap())
+            .collect();
+        let repeated_springs: String = vec![spring_config; 5].join("?");
+        springs.push(repeated_springs);
+        numbers.push(number_config.repeat(5))
+    });
+
+    return (springs, numbers);
+}
+
+fn combinations(
+    springs: String,
+    numbers: &Vec<i64>,
+    cache: &mut HashMap<(String, Vec<i64>), i64>,
+) -> i64 {
+    if springs == "" {
+        return if numbers.is_empty() { 1 } else { 0 };
+    }
+
+    if numbers.is_empty() {
+        return if springs.contains("#") { 0 } else { 1 };
+    }
+
+    let key: (String, Vec<i64>) = (springs.clone(), numbers.clone());
+    if cache.contains_key(&key) {
+        return *cache.get(&key).unwrap();
+    }
+
+    let mut ret: i64 = 0;
+
+    let first_el: char = springs.chars().nth(0).unwrap();
+    if ".?".contains(first_el) {
+        ret += combinations(springs.chars().skip(1).collect(), numbers, cache);
+    }
+
+    if "#?".contains(first_el) {
+        let n_0: usize = numbers[0] as usize;
+        if n_0 <= springs.len()
+            && !(springs.chars().take(n_0).collect::<String>().contains('.'))
+            && (springs.len() == n_0 || springs.chars().nth(n_0).unwrap() != '#')
+        {
+            ret += combinations(
+                springs.chars().skip(n_0 + 1).collect(),
+                &numbers.iter().skip(1).cloned().collect(),
+                cache,
+            );
+        }
+    }
+
+    cache.insert(key, ret);
+
+    ret
 }
 
 pub fn main() {
-    println!("Part B")
+    let (springs, numbers) = read_file("../input.txt");
+    let mut results: Vec<i64> = Vec::new();
+    let mut cache: HashMap<(String, Vec<i64>), i64> = HashMap::new();
+    for i in 0..springs.len() {
+        let res = combinations(springs[i].to_string(), &numbers[i], &mut cache);
+        results.push(res);
+    }
+    let sum: i64 = results.iter().sum();
+    println!("Result: {}", sum);
 }
